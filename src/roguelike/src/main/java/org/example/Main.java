81f2b802e28788ca4041d99342f9a5caec192886
barley <rf_3088@mail.ru>
2026-01-20 15:36:40 +0300

    merge

--- src/roguelike/src/main/java/org/example/Main.java
+++ src/roguelike/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ import com.googlecode.lanterna.input.KeyType;
 import domain.Model;
 import domain.enums.StatusPlayer;
 import presentation.Controller;
-import presentation.View;
+import p    resentation.View;
 
 import java.io.IOException;
 
